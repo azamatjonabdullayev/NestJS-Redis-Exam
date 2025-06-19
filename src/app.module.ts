dc551b9ac6b5f407e67fdbad9cb0f46e2cf56fd5
Azamat <abdullayevazamat2705@gmail.com>
2025-06-20 02:44:35 +0500

    generated movie module and temporary deleted users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { MoviesModule } from './modules/movies/movies.module';
 
 @Module({
-  imports: [AuthModule, CoreModule, UsersModule],
+  imports: [AuthModule, CoreModule, UsersModule, MoviesModule],
 })
 export class AppModule {}
