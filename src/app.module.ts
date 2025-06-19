be782fd1b0d815f4cbf6cd29eb6e51b3f661ce42
Azamat <abdullayevazamat2705@gmail.com>
2025-06-19 20:42:57 +0500

    Created user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { EmailModule } from './modules/email/email.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [AuthModule, CoreModule, EmailModule],
+  imports: [AuthModule, CoreModule, EmailModule, UsersModule],
 })
 export class AppModule {}
