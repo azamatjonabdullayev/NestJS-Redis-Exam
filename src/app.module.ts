4bfa37e47e98744190865ee064c3382728570e88
Azamat <abdullayevazamat2705@gmail.com>
2025-06-20 03:08:47 +0500

    Returned user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { CoreModule } from './core/core.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { MoviesModule } from './modules/movies/movies.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [AuthModule, CoreModule, MoviesModule],
+  imports: [AuthModule, CoreModule, MoviesModule, UsersModule],
 })
 export class AppModule {}
